bf6f9bf2c499a887a35593a91651b5b16e0afd6a
kikuchiyo <jimenez.john0@gmail.com>
2013-10-23 17:18:03 -0700

    Implement Range Selection Movement, and use calvary unit icons for the units.

--- app/assets/javascripts/easeljs_canvas_practice.js
+++ app/assets/javascripts/easeljs_canvas_practice.js
@@ -1,4 +1,47 @@
 // For Example 02
+
+function climbing_mountain(coordinates){
+  var climbingMountain = false;
+  _.each( App.mountains, function(mountain){
+    if( mountain.x == coordinates.x && mountain.y == coordinates.y ){
+      climbingMountain = true
+    }
+  });
+  return climbingMountain;
+} 
+
+function add_movement_button(button, desired_x, desired_y){
+  movement_buttons.push( stage.addChild(new Button(' ', '#95B')))
+  movement_buttons[movement_buttons.length - 1].x = desired_x;
+  movement_buttons[movement_buttons.length - 1].y = desired_y;
+}
+
+function can_move_west(desired_x, desired_y) { return desired_x >= 0 && climbing_mountain({x: desired_x, y:desired_y}) == false; }
+function can_move_north(desired_x, desired_y){ return desired_y >= 0 && climbing_mountain({x: desired_x, y:desired_y}) == false; }
+
+function can_move_east(desired_x, desired_y) { return desired_x < stage.canvas.width && climbing_mountain({x: desired_x, y:desired_y}) == false; }
+function can_move_south(desired_x, desired_y){ return desired_y < stage.canvas.height && climbing_mountain({x: desired_x, y:desired_y}) == false; }
+
+function can_move_north_west(desired_x, desired_y){ return desired_x >= 0 && desired_y >= 0 && climbing_mountain({x: desired_x, y:desired_y}) == false; }
+function can_move_south_west(desired_x, desired_y){ return desired_x >= 0 && desired_y < stage.canvas.height && climbing_mountain({x: desired_x, y:desired_y}) == false; }
+
+function can_move_south_east(desired_x, desired_y){ return desired_x < stage.canvas.width && desired_y < stage.canvas.height && climbing_mountain({x: desired_x, y:desired_y}) == false; }
+function can_move_north_east(desired_x, desired_y){ return desired_x < stage.canvas.width && desired_y >= 0 && climbing_mountain({x: desired_x, y:desired_y}) == false; }
+
+function create_movement_tiles(button, infantry_ranges){
+  _.each(infantry_ranges, function(adjustment, i){
+    (function(desired_x, desired_y){ if (can_move_west(desired_x, desired_y)) { add_movement_button(button, desired_x, desired_y) } })(button.x - adjustment, button.y);
+    (function(desired_x, desired_y){ if (can_move_east(desired_x, desired_y) ){ add_movement_button(button, desired_x, desired_y) } })(button.x + adjustment, button.y);
+    (function(desired_x, desired_y){ if (can_move_south(desired_x, desired_y)){ add_movement_button(button, desired_x, desired_y) } })(button.x, button.y + adjustment);
+    (function(desired_x, desired_y){ if (can_move_north(desired_x, desired_y)){ add_movement_button(button, desired_x, desired_y) } })(button.x, button.y - adjustment);
+    if(i + 1 <= infantry_ranges.length / 2){
+      (function(desired_x, desired_y){ if (can_move_north_west(desired_x, desired_y)){ add_movement_button(button, desired_x, desired_y) } })(button.x - adjustment, button.y - adjustment);
+      (function(desired_x, desired_y){ if (can_move_south_east(desired_x, desired_y)){ add_movement_button(button, desired_x, desired_y) } })(button.x + adjustment, button.y + adjustment);
+      (function(desired_x, desired_y){ if (can_move_north_east(desired_x, desired_y)){ add_movement_button(button, desired_x, desired_y) } })(button.x + adjustment, button.y - adjustment);
+      (function(desired_x, desired_y){ if (can_move_south_west(desired_x, desired_y)){ add_movement_button(button, desired_x, desired_y) } })(button.x - adjustment, button.y + adjustment);
+    }
+  });
+}
 (function() {
 
   var Button = function(label, color) {
@@ -23,8 +66,8 @@
     text.textBaseline = "top";
     text.textAlign = "center";
     
-    var width = 32; //text.getMeasuredWidth()+30;
-    var height = 32; //text.getMeasuredHeight()+20;
+    var width = 32;
+    var height = 32;
     
     this.background = new createjs.Shape();
     this.background._province_name = 'Hei Fei';
@@ -45,19 +88,20 @@
   } 
 
   p.handleClick = function (event) {    
-    var target = event.target;
-    console.log(target);
-    alert("You clicked on a button: " + target._province_name);
+    var target = event.target.parent;
+
+    if(target.x < btn2.x){ btn2.gotoAndPlay('left'); } else if(target.x > btn2.x){ btn2.gotoAndPlay('right'); }
+    btn2.x = target.x; 
+    btn2.y = target.y;
+
+    _.each( movement_buttons, function(button){ stage.removeChild(button); });
+    create_movement_tiles(btn2, [32, 64]);
   } 
   
-  p.handleTick = function(event) {       
-    p.alpha = Math.cos(p.count++*0.1)*0.4+0.6;
-  }
-  
+  p.handleTick = function(event) { p.alpha = Math.min(Math.cos(p.count++*0.5)*0.4+0.8, 0.6); }
   window.Button = Button;
 }());
 
-var stage, holder;
 function example_02() {
   stage = new createjs.Stage("game_map");
 
@@ -66,52 +110,29 @@ function example_02() {
   tileset.src = mapData.tilesets[0].image;
   tileset.onLoad = initLayers(stage); 
 
-  var btn1 = stage.addChild(new Button("100", "#F00"));
+  var data = {
+    images: ["assets/calvalry-unit-sprite-sheet.png"],
+    frames: {width:32, height:32},
+    animations: {left:[0], right:[1]}
+  };
+
+  var spriteSheet = new createjs.SpriteSheet(data);
+
+  btn2 = stage.addChild(new createjs.Sprite(spriteSheet, "left"));
+  btn2.x = 192;
+  btn2.y = 224;
+
+  var btn1 = stage.addChild(new createjs.Sprite(spriteSheet, "right"));
   btn1.x = 64;
   btn1.y = 0;
+  movement_buttons = [];
 
-  var btn2 = stage.addChild(new Button("57", "#0F0"));
-  btn2.x = 64;
-  btn2.y = 96;
+  create_movement_tiles(btn2, [32, 64]);
+
   createjs.Ticker.addEventListener("tick", stage);
   canvas = document.getElementById('game_controller')
-
-  function climbing_mountain(coordinates){
-    var climbingMountain = false;
-    _.each( App.mountains, function(mountain){
-      if( mountain.x == coordinates.x && mountain.y == coordinates.y ){
-        climbingMountain = true
-      }
-    });
-    return climbingMountain;
-  } 
-
-  $(window).keydown(function(e){
-    var desired_x = btn1.x
-    var desired_y = btn1.y
-
-    if(e.keyCode == 37){
-      e.preventDefault();
-      desired_x -= 32;
-      if (desired_x >= 0 && climbing_mountain({x: desired_x, y:desired_y}) == false){ btn1.x = desired_x; }
-    } else if(e.keyCode == 38){
-      e.preventDefault();
-      desired_y -= 32;
-      if (desired_y >= 0 && climbing_mountain({x: desired_x, y:desired_y}) == false){ btn1.y = desired_y; }
-    } else if(e.keyCode == 39){
-      e.preventDefault();
-      desired_x += 32;
-      if ( desired_x < stage.canvas.width && climbing_mountain({x: desired_x, y:desired_y}) == false){ 
-        btn1.x = desired_x; 
-      }
-    } else if(e.keyCode == 40){
-      e.preventDefault();
-      desired_y += 32;
-      if ( desired_y < stage.canvas.height && climbing_mountain({x: desired_x, y:desired_y}) == false){ btn1.y = desired_y; }
-    }
-  });
+  //sparkle_init();
 }
 
 function init(){ example_02(); }
-
 $(function(){ init(); })
---
+++ app/assets/javascripts/sparkles.js
@@ -0,0 +1,108 @@
+
+  var imgSeq = new Image();        // The image for the sparkle animation
+  var sprite;                        // The animated sparkle template to clone
+  var fpsLabel;
+
+  function sparkle_init() {
+    // create a new stage and point it at our canvas:
+    //canvas = document.getElementById("game_map");
+    //stage = new createjs.Stage(canvas);
+    // attach mouse handlers directly to the source canvas.
+    // better than calling from canvas tag for cross browser compatibility:
+    stage.addEventListener("stagemousemove", moveCanvas);
+    stage.addEventListener("stagemousedown", clickCanvas);
+
+    // define simple sprite sheet data specifying the image(s) to use, the size of the frames,
+    // and the registration point of the frame
+    // it will auto-calculate the number of frames from the image dimensions and loop them
+    var data = {
+        images:["assets/sparkle_21x23.png"],
+        frames:{width:21, height:23, regX:10, regY:11}
+    }
+
+    // set up an animation instance, which we will clone
+    sprite = new createjs.Sprite(new createjs.SpriteSheet(data));
+
+    // add a text object to output the current FPS:
+    fpsLabel = new createjs.Text("-- fps", "bold 14px Arial", "#FFF");
+    stage.addChild(fpsLabel);
+    fpsLabel.x = 10;
+    fpsLabel.y = 20;
+
+    // start the tick and point it at the window so we can do some work before updating the stage:
+    createjs.Ticker.setFPS(20);
+    createjs.Ticker.addEventListener("tick", tick);
+  }
+
+
+  function tick(event) {
+      // loop through all of the active sparkles on stage:
+      var canvas = document.getElementById('game_map');
+      var l = stage.getNumChildren();
+      for(var i = l - 1; i > 0; i--) {
+          var sparkle = stage.getChildAt(i);
+          if (sparkle.name != 'SPARKLE'){break;}
+          // apply gravity and friction
+          sparkle.vY += 2;
+          sparkle.vX *= 0.98;
+
+          // update position, scale, and alpha:
+          sparkle.x += sparkle.vX;
+          sparkle.y += sparkle.vY;
+          sparkle.scaleX = sparkle.scaleY = sparkle.scaleX + sparkle.vS;
+          sparkle.alpha += sparkle.vA;
+
+          //remove sparkles that are off screen or not invisble
+          if (sparkle.alpha <= 0 || sparkle.y > canvas.height) {
+              stage.removeChildAt(i);
+          }
+      }
+
+      fpsLabel.text = Math.round(createjs.Ticker.getMeasuredFPS()) + " fps";
+
+      // draw the updates to stage
+      stage.update(event);
+  }
+
+  //sparkle explosion
+  function clickCanvas(evt) {
+      addSparkles(Math.random() * 200 + 100 | 0, stage.mouseX, stage.mouseY, 2);
+  }
+
+  //sparkle trail
+  function moveCanvas(evt) {
+      addSparkles(Math.random() * 2 + 1 | 0, stage.mouseX, stage.mouseY, 1);
+  }
+
+  function addSparkles(count, x, y, speed) {
+      //create the specified number of sparkles
+      for(var i = 0; i < count; i++) {
+          // clone the original sparkle, so we don't need to set shared properties:
+          var sparkle = sprite.clone();
+
+          // set display properties:
+          sparkle.x = x;
+          sparkle.y = y;
+          sparkle.name = "SPARKLE";
+          //sparkle.rotation = Math.random()*360;
+          sparkle.alpha = Math.random() * 0.5 + 0.5;
+          sparkle.scaleX = sparkle.scaleY = Math.random() + 0.3;
+
+          // set up velocities:
+          var a = Math.PI * 2 * Math.random();
+          var v = (Math.random() - 0.5) * 30 * speed;
+          sparkle.vX = Math.cos(a) * v;
+          sparkle.vY = Math.sin(a) * v;
+          sparkle.vS = (Math.random() - 0.5) * 0.2; // scale
+          sparkle.vA = -Math.random() * 0.05 - 0.01; // alpha
+
+          // start the animation on a random frame:
+          sparkle.gotoAndPlay(Math.random() * sparkle.spriteSheet.getNumFrames());
+
+          // add to the display list:
+          stage.addChild(sparkle);
+      }
+  }
+
+
+
